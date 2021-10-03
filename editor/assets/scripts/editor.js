daa95d96259cea4cbfac82a70bdcf12ff3e3bf99
geekboysupreme <shuvam.manna@gmail.com>
2021-10-03 14:42:58 +0530

    editor baby steps - 2

--- editor/assets/scripts/editor.js
+++ editor/assets/scripts/editor.js
@@ -37,3 +37,49 @@ function deleteObject() {
     var obj = canvas.getActiveObject();
     canvas.remove(obj);
 }
+
+
+function addImage() {
+    var img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1KOgrpDSvLR_YsBx7k6ygmKP-C4g38R_HRQ&usqp=CAU';
+    img.onload = function() {
+        var imgInstance = new fabric.Image(img, {
+            left: 100,
+            top: 100,
+            angle: 0,
+            opacity: 1
+        });
+        canvas.add(imgInstance);
+    };
+}
+
+
+function saveImage() {
+    var canv = document.getElementById('canvas');
+    canv.toBlob(function(blob) {
+        console.log(blob);
+        saveAs(blob, 'my-image.png');
+    });
+}
+
+function setBackground() {
+    var img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1KOgrpDSvLR_YsBx7k6ygmKP-C4g38R_HRQ&usqp=CAU';
+
+    img.onload = function() {
+        var imgInstance = new fabric.Image(img, {
+            left: 0,
+            top: 0,
+            angle: 0,
+            opacity: 1
+        });
+        //canvas.add(imgInstance);
+        canvas.setBackgroundImage(imgInstance, canvas.renderAll.bind(canvas), {
+            scaleX: canvas.width / img.width,
+            scaleY: canvas.height / img.height
+         });
+
+}
+}
