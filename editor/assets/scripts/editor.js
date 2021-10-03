a98b186c661968d69f01e14653dd17624f92eb8f
geekboysupreme <shuvam.manna@gmail.com>
2021-10-03 13:35:35 +0530

    editor baby steps - 2

--- editor/assets/scripts/editor.js
+++ editor/assets/scripts/editor.js
@@ -1,13 +1,39 @@
 var canvas = new fabric.Canvas('canvas');
 
-var rect = new fabric.Rect({
-    left: 100,
-    top: 100,
-    fill: 'red',
-    width: 20,
-    height: 20
-});
 
-canvas.add(rect);
+function drawRectangle() {
+    var rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'grey',
+        width: 60,
+        height: 60
+    });
+    
+    canvas.add(rect);    
+}
 
-console.log(rect);
+
+
+function drawCircle() {
+    var circ = new fabric.Circle({
+        left: 100,
+        top: 100,
+        fill: 'grey',
+        radius: 30
+    });
+    
+    canvas.add(circ);    
+}   
+
+
+function changeColor() {
+    var obj = canvas.getActiveObject();
+    obj.set('fill', 'blue');
+    canvas.renderAll();
+}
+
+function deleteObject() {
+    var obj = canvas.getActiveObject();
+    canvas.remove(obj);
+}
