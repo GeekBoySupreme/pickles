f7d85d2d12ef581f13b38b042116d0dec767ce84
geekboysupreme <shuvam.manna@gmail.com>
2021-10-06 00:49:52 +0530

    Connected to Supabase yoohoo

--- editor/assets/scripts/editor.js
+++ editor/assets/scripts/editor.js
@@ -83,3 +83,30 @@ function setBackground() {
 
 }
 }
+
+
+
+
+
+
+
+
+
+
+const { createClient } = supabase
+supabase = createClient("https://jxywbtgddxjqlcwoioah.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzQ1NTIzMCwiZXhwIjoxOTQ5MDMxMjMwfQ.or_Vm-5JnsYH0BFHoRqND6ykaUwpSfo1VREUAB6S5YY");
+
+
+async function createData() {
+    const { data, error} = await supabase
+    .from('pickle-gallery')
+    .insert([
+        {
+            image_data: canvas.toDataURL(),
+            name: 'Pickle Rick',
+            social_handle: '@picklerick',
+            remix: 0,
+            featured: 1
+        }
+    ])
+}
---
+++ gallery/scripts/caller.js
@@ -0,0 +1,34 @@
+const { createClient } = supabase
+supabase = createClient("https://jxywbtgddxjqlcwoioah.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzQ1NTIzMCwiZXhwIjoxOTQ5MDMxMjMwfQ.or_Vm-5JnsYH0BFHoRqND6ykaUwpSfo1VREUAB6S5YY");
+
+
+async function createData() {
+    const { data, error} = await supabase
+    .from('pickle-gallery')
+    .insert([
+        {
+            name: 'Pickle Rick',
+            social_handle: '@picklerick',
+            remix: 0,
+            featured: 1
+        }
+    ])
+}
+
+
+async function getData() {
+    const { data, error} = await supabase
+    .from('pickle-gallery')
+    .select()
+    .then(data => {
+        renderImages(data);
+    })
+}
+
+function renderImages(data) {
+    console.log(data.body[0].image_data);
+
+    var image = document.createElement('img');
+    image.src = data.body[0].image_data;
+    document.body.appendChild(image);
+}
